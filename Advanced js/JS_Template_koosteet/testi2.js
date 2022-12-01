02299b63e8959020fc10f65e3458dc22038e156f
n2jato00 <n2jato00@students.oamk.fi>
2022-12-01 10:51:22 +0200

    tuntitehtävä

--- Advanced js/JS_Template_koosteet/testi2.js
+++ Advanced js/JS_Template_koosteet/testi2.js
@@ -14,3 +14,17 @@ persons.push(person)
 for (p of persons) {
     console.log(p.age)
 }
+
+let car = {
+    plateNr: "OOP-001",
+    currentSpeed: 120
+}
+console.log(car)
+
+let car2 = {
+    plateNr: "KUR-597",
+    currentSpeed: 160
+}
+console.log(car2)
+car2.currentSpeed = 140;
+console.log(car2)
