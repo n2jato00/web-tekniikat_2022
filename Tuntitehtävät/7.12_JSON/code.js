09e6605134d0cd1ea81cd94e46415a3896686441
n2jato00 <n2jato00@students.oamk.fi>
2022-12-07 15:22:52 +0200

    JSON update

--- Tuntitehtävät/7.12_JSON/code.js
+++ Tuntitehtävät/7.12_JSON/code.js
@@ -1,10 +1,29 @@
 getData();
+getData1();
 /* ei jäädä odottamaan, tehdään muuta, esim. lisätään vaikka elementtejä sivulle */
 
 /* async sisältää toimintaa jota ei jäädä odottamaan */
 async function getData(){
-   const RESPONSE = await fetch("https://random-data-api.com/api/v2/appliances")
+   const RESPONSE = await fetch("https://random-data-api.com/api/v2/appliances?size=10")
    const OLIO = await RESPONSE.json();
 
-   console.log(OLIO)
+   for( laite of OLIO){
+    let h = document.createElement("h4");
+    h.textContent = laite.equipment;
+    document.body.appendChild(h);
+   }
+}
+
+
+function getData1(){
+    fetch("https://random-data-api.com/api/v2/appliances?size=10")
+        .then( resp => resp.json())
+        .then( laitteet => {
+            for( laite of laitteet){
+                let h = document.createElement("h4");
+                h.textContent = laite.equipment;
+                document.body.appendChild(h);
+               }
+        })
+
 }
